4c185bb0661e1cb6b94c53543c0678004821c856
garymor <garymor.il@gmail.com>
2023-07-24 23:41:53 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import '../scss/style.scss'
+import logo_img from '../../afec_logo.jpeg'
+import product_1 from '../../product_1.jpeg'
+
+const img = document.createElement('img')
+img.width = 90
+img.src = logo_img
+img.classList="img-fluid"
+const logo_div = document.querySelector('#logo_div')
+logo_div.appendChild(img)
+
+const img_prd1 = document.createElement('img')
+img_prd1.width = 200
+img_prd1.src = product_1
+img_prd1.classList="img-fluid rounded-start"
+const prd1_div = document.querySelector('#product_1')
+prd1_div.appendChild(img_prd1)
+
