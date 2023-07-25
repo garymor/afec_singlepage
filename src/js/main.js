3907a1fc816109b2c8bd07a3fa74ef26f412d262
garymor <garymor.il@gmail.com>
2023-07-25 17:42:30 +0300

    crrnt

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,18 @@ import * as bootstrap from 'bootstrap'
 import '../scss/style.scss'
 import logo_img from '../../afec_logo.jpeg'
 import product_1 from '../../product_1.jpeg'
+import product_2 from '../../product_2.jpeg'
+
+function addImage(slct,img) {
+    let img_prd = document.createElement('img')
+    img_prd.width = 300
+    img_prd.src = img
+    img_prd.classList="img-fluid rounded-start"
+    let prd_div = document.querySelector(slct)
+    prd_div.appendChild(img_prd)
+}
+
+addImage('#product_2',product_2)
 
 const img = document.createElement('img')
 img.width = 90
@@ -20,6 +32,9 @@ img_prd1.classList="img-fluid rounded-start"
 const prd1_div = document.querySelector('#product_1')
 prd1_div.appendChild(img_prd1)
 
+
+
+
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
@@ -31,4 +46,20 @@ if (toastTrigger) {
 }
 
 
+
+// const card_prd1 = document.getElementById('card_prd1')
+
+// function expand(that) {
+//     that.classList.add('bigger')
+//     that.classList.remove('original')
+// }
+
+// function unexpand(that) {
+//     that.classList.add('original')
+//     that.classList.remove('bigger')
+// }
+  
+// window.expand = expand
+// window.unexpand = unexpand
+ 
 
